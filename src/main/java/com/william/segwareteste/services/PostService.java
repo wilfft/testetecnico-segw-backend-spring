fae50722d0e4985d97059542f2fb1379a2b41c93
wilfft <68745408+wilfft@users.noreply.github.com>
2021-03-05 16:43:51 -0300

    feat: service> criado salvarPostagem()

--- src/main/java/com/william/segwareteste/services/PostService.java
+++ src/main/java/com/william/segwareteste/services/PostService.java
@@ -1,4 +1,29 @@
 package com.william.segwareteste.services;
 
+import com.william.segwareteste.entity.Post;
+import com.william.segwareteste.repository.PostRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.List;
+
 public class PostService {
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Post> receberPosts() {
+        return this.postRepository.findByOrderByHorarioPostagemDescDesc();
+    }
+
+    @Transactional
+    public Post salvarPostagem(Post post) {
+        post.setHorarioPostagem(Instant.now());
+        post.setUpvotes(0);
+        return postRepository.save(post)
+    }
+
 }
