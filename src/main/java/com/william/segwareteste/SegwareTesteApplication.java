14054b052ffc59de19c131900d1ecef9f5a159c1
wilfft <68745408+wilfft@users.noreply.github.com>
2021-03-05 16:02:41 -0300

    iniciando projeto backend

---
+++ src/main/java/com/william/segwareteste/SegwareTesteApplication.java
@@ -0,0 +1,13 @@
+package com.william.segwareteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegwareTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegwareTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/william/segwareteste/SegwareTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.segwareteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegwareTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
