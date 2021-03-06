d7055c740b9656687b93947c677f775d86b941bd
wilfft <68745408+wilfft@users.noreply.github.com>
2021-03-06 00:08:49 -0300

    adiocado cors para liberar acessos

---
+++ src/main/java/com/william/segwareteste/dto/PostDTO.java
@@ -0,0 +1,12 @@
+package com.william.segwareteste.dto;
+
+import java.time.Instant;
+
+public class PostDTO {
+
+    private Long id;
+    private String autor;
+    private String conteudo;
+    private Instant horarioPostagem;
+    private Integer upvotes;
+}
---
+++ src/main/java/com/william/segwareteste/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.william.segwareteste.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    CorsConfigurationSource corsConfiguration() {
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTION"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
